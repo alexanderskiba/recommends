9c975b0d769a0e941760a291b54346dbc3c9d27d
Alexander Skiba <vetrolet3000@gmail.com>
2021-03-02 20:54:12 +0300

    Beginning of work

---
+++ main.py
@@ -0,0 +1,12 @@
+import csv
+import bisect
+
+
+class Handler:
+    """Класс-обработчик"""
+    def __init__(self, sku, probability=None):
+        self.sku = sku
+        self.probability = probability
+
+    def process(self):
+        """основной метод для обработки файла"""
