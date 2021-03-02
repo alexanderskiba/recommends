ca4a150b2c80ea899035eb7ccb29bdea26e22848
Alexander Skiba <vetrolet3000@gmail.com>
2021-03-02 23:28:39 +0300

    nothing useless

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     reader = Reader("rec.csv")
     data = reader.read_csv()
     # создадим объект обработчика
-    text_handler = Handler("QgybZB43EN", 0.9)
+    text_handler = Handler("QgybZB43EN") # 0.8 0.9
     # запустим метод для целевой обработки(возврата списка рекомендаций для указанного товара)
     target_list = text_handler.process(data)
     print(target_list)
